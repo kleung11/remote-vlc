ec5e94bdb82e99de73bac2360340b3a53e7044de
Ricky Leung <kleung11@gmail.com>
2016-06-26 10:21:20 -0400

    Add missing files after refactoring.

---
+++ root/configs.php
@@ -0,0 +1,15 @@
+<?php
+
+// db configs
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "usbw";
+$db_name = "kko";
+
+// vlc configs
+$vlc_site = $_SERVER['SERVER_NAME'];
+$vlc_port = "8080";
+$vlc_username = "";
+$vlc_password = "ricky";
+
+?>
---
+++ root/js/settings.js
@@ -0,0 +1,25 @@
+/*
+function processSettings($settings) {
+	console.log($settings);
+	console.log($settings.information.category);
+	
+	if ($settings !== null && $settings !== undefined) {
+		$.each( $settings.information.category, function( key, value ) {
+			if (value.Type == 'Audio') {
+				alert(key.match(/\d/));
+				 $("#audio-setting-" . key.match(/\d/)).
+			}
+		});
+	}
+	$( "#settings-menu" ).load( 'http://' + location.hostname + '/requests/playlist.php', function() {});
+}
+*/
+/*
+$(function() {
+	$('#settings').click(function() {
+		getStatus();
+	});
+	
+	$('#settings').click();
+});
+*/
